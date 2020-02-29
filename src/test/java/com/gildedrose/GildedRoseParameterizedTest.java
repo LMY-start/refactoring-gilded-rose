ec21a02707a2598dc810ff700df90699f8462c0a
YONGMA <yong.ma@thoughtworks.com>
2020-02-29 12:32:06 +0800

    为name = "Elixir of the Mongoose"的GildedRose添加测试用例，并添加测试出错时的message提示

--- src/test/java/com/gildedrose/GildedRoseParameterizedTest.java
+++ src/test/java/com/gildedrose/GildedRoseParameterizedTest.java
@@ -7,7 +7,8 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
 public class GildedRoseParameterizedTest {
@@ -41,6 +42,13 @@ public class GildedRoseParameterizedTest {
                 {new RoseItem[]{new RoseItem("Aged Brie", 0, 0)}, "Aged Brie", -1, 2},
                 {new RoseItem[]{new RoseItem("Aged Brie", 0, 2)}, "Aged Brie", -1, 4},
 
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 5, 9)}, "Elixir of the Mongoose", 4, 8},
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 1, 1)}, "Elixir of the Mongoose", 0, 0},
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 1, 0)}, "Elixir of the Mongoose", 0, 0},
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 0, 0)}, "Elixir of the Mongoose", -1, 0},
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 0, 2)}, "Elixir of the Mongoose", -1, 0},
+                {new RoseItem[]{new RoseItem("Elixir of the Mongoose", 0, 5)}, "Elixir of the Mongoose", -1, 3},
+
                 {new RoseItem[]{new RoseItem("foo", 1, 2)}, "foo", 0, 1},
                 {new RoseItem[]{new RoseItem("foo", 0, 2)}, "foo", -1, 0},
                 {new RoseItem[]{new RoseItem("foo", 0, 5)}, "foo", -1, 3},
@@ -53,8 +61,15 @@ public class GildedRoseParameterizedTest {
 
         gildedRose.updateQuality();
 
-        assertEquals(exceptedRoseName, gildedRose.roseItems[0].name);
-        assertEquals(exceptedSellIn, gildedRose.roseItems[0].sellIn);
-        assertEquals(exceptedQuality, gildedRose.roseItems[0].quality);
+        String name = gildedRose.roseItems[0].name;
+        String messageFormat = "Test for " + name + " of %s, the actual value is %s, but excepted value is %s";
+        assertThat(String.format(messageFormat, "name", name, exceptedRoseName),
+                   name, is(exceptedRoseName));
+        int sellIn = gildedRose.roseItems[0].sellIn;
+        assertThat(String.format(messageFormat, "sellIn", sellIn, +exceptedSellIn),
+                   sellIn, is(exceptedSellIn));
+        int quality = gildedRose.roseItems[0].quality;
+        assertThat(String.format(messageFormat, "quality", quality, exceptedQuality),
+                   quality, is(exceptedQuality));
     }
 }
