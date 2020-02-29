e7c5c941da185725028d30ff5a48b5aa6fecb195
YONGMA <yong.ma@thoughtworks.com>
2020-02-29 18:52:14 +0800

    识别依恋情节味道，把处理roseItem的方法都放到RoseItem类中

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,57 +16,14 @@ class GildedRose {
     }
 
     private void processQuality(RoseItem roseItem) {
-        if (!roseItem.getName().equals("Aged Brie")
-                && !roseItem.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (roseItem.getQuality() > 0) {
-                if (!roseItem.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                    roseItem.decreaseQuality();
-                }
-            }
-        } else {
-            if (roseItem.getQuality() < 50) {
-                roseItem.increaseQuality();
-
-                if (roseItem.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (roseItem.getSellIn() < 11) {
-                        if (roseItem.getQuality() < 50) {
-                            roseItem.increaseQuality();
-                        }
-                    }
-
-                    if (roseItem.getSellIn() < 6) {
-                        if (roseItem.getQuality() < 50) {
-                            roseItem.increaseQuality();
-                        }
-                    }
-                }
-            }
-        }
+        roseItem.processQuality();
     }
 
     private void processSellIn(RoseItem roseItem) {
-        if (!roseItem.getName().equals("Sulfuras, Hand of Ragnaros")) {
-            roseItem.decreaseSellIn();
-        }
+        roseItem.processSellIn();
     }
 
     private void processSellInLessZero(RoseItem roseItem) {
-        if (roseItem.getSellIn() < 0) {
-            if (!roseItem.getName().equals("Aged Brie")) {
-                if (!roseItem.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (roseItem.getQuality() > 0) {
-                        if (!roseItem.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                            roseItem.decreaseQuality();
-                        }
-                    }
-                } else {
-                    roseItem.resetQuality2Zero();
-                }
-            } else {
-                if (roseItem.getQuality() < 50) {
-                    roseItem.increaseQuality();
-                }
-            }
-        }
+        roseItem.processSellInLessZero();
     }
 }
--- src/main/java/com/gildedrose/RoseItem.java
+++ src/main/java/com/gildedrose/RoseItem.java
@@ -16,24 +16,79 @@ public class RoseItem {
         this.quality = quality;
     }
 
-    public void decreaseSellIn() {
+    private void decreaseSellIn() {
         sellIn -= 1;
     }
 
-    public void increaseQuality() {
+    private void increaseQuality() {
         quality += 1;
     }
 
-    public void decreaseQuality() {
+    private void decreaseQuality() {
         quality -= 1;
     }
 
-    public void resetQuality2Zero() {
+    private void resetQuality2Zero() {
         quality = 0;
     }
 
     @Override
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    public void processQuality() {
+        if (!name.equals("Aged Brie")
+                && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decreaseQuality();
+                }
+            }
+        } else {
+            if (quality < 50) {
+                increaseQuality();
+
+                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            increaseQuality();
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            increaseQuality();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void processSellIn() {
+        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+            decreaseSellIn();
+        }
+    }
+
+    public void processSellInLessZero() {
+        if (sellIn < 0) {
+            if (!name.equals("Aged Brie")) {
+                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (quality > 0) {
+                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decreaseQuality();
+                        }
+                    }
+                } else {
+                    resetQuality2Zero();
+                }
+            } else {
+                if (quality < 50) {
+                    increaseQuality();
+                }
+            }
+        }
     }
 }
